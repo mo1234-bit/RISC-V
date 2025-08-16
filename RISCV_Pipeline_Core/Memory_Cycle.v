1f46a9dfcd9203395148ecffdb18d7fcaf517f71
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-08-16 19:37:00 +0300

    Update Memory_Cycle.v

--- RISCV_Pipeline_Core/Memory_Cycle.v
+++ RISCV_Pipeline_Core/Memory_Cycle.v
@@ -11,94 +11,95 @@ module memory_cycle(input clk, rst, RegWriteM,FRegWriteM, MemWriteM, ResultSrcM,
     reg RegWriteM_r,FRegWriteM_r, ResultSrcM_r,FPU_ResultEM_r;
     reg [4:0] RD_M_r;
     reg [31:0] PCPlus4M_r, ALU_ResultM_r, ReadDataM_r;
-    assign o_p_waitrequest=0;
-//     wire  [24:0]     i_p_addr;
-//           wire [3:0]    i_p_byte_en;
-//           wire [31:0]    i_p_writedata;
-//           wire  i_p_read;
-//           wire    i_p_write;
-//           wire   [31:0]  o_p_readdata;
-//           wire     o_p_readdata_valid;
+    
+    wire  [24:0]     i_p_addr;
+          wire [3:0]    i_p_byte_en;
+          wire [31:0]    i_p_writedata;
+          wire  i_p_read;
+          wire    i_p_write;
+          wire   [31:0]  o_p_readdata;
+          wire     o_p_readdata_valid;
        
 
-//           wire   [25:0]  o_m_addr;
-//           wire   [3:0]  o_m_byte_en;
-//           wire     o_m_read;
-//           wire     o_m_write;
-//           wire     i_m_readdata_valid;
-//           wire     i_m_waitrequest;
+          wire   [25:0]  o_m_addr;
+          wire   [3:0]  o_m_byte_en;
+          wire     o_m_read;
+          wire     o_m_write;
+          wire     i_m_readdata_valid;
+          wire     i_m_waitrequest;
 
-//           wire  [31:0]   cnt_r;
-//           wire   [31:0]  cnt_w;
-//           wire  [31:0]   cnt_hit_r;
-//           wire  [31:0]   cnt_hit_w;
-//           wire  [31:0]   cnt_wb_r;
-//           wire  [31:0]   cnt_wb_w;
-//           wire [4:0]rs1,rs2;
-//           assign i_p_write=MemWriteM;
-//           assign i_p_read=(MemWriteM==0);
-//           assign i_p_writedata=WriteDataM;
-//           assign {rs2,rs1}=ALU_ResultM[11:2];
-//           wire [31:0]data_out;
-//           wire[31:0]data_in;
-//           address dut1(
-//      MemWriteM,
-//      clk,    
-//      rst_n,  
-//     (i_p_write||i_p_read),  
-//     rs1,
-//     rs2,
-//     i_p_addr,
-//     i_p_byte_en
-// );
-
-//     cache dut(clk,
-//                rst,
-//                i_p_addr,
-//                i_p_byte_en,
-//                i_p_writedata,
-//                i_p_read,
-//                i_p_write,
-//                ReadDataM,
-//                o_p_readdata_valid,
-//                o_p_waitrequest,
-//                o_m_addr,
-//                o_m_byte_en,
-//                data_in,
-//                o_m_read,
-//                o_m_write,
-//                data_out,
-//                i_m_readdata_valid,
-//                i_m_waitrequest,
-//                cnt_r,
-//                cnt_w,
-//                cnt_hit_r,
-//                cnt_hit_w,
-//                cnt_wb_r,
-//                cnt_wb_w);
-    // Data_Memory dmem (
-    //                     .clk(clk),
-    //                     .rst(rst),
-    //                     .WE(o_m_write),
-    //                     .WD(data_in),
-    //                     .i_m_readdata_valid(i_m_readdata_valid),
-    //                     .i_m_waitrequest(i_m_waitrequest),
-    //                     .ren(o_m_read),
-    //                     .A(ALU_ResultM),
-    //                     .RD(data_out)
-    //                 );
-
+          wire  [31:0]   cnt_r;
+          wire   [31:0]  cnt_w;
+          wire  [31:0]   cnt_hit_r;
+          wire  [31:0]   cnt_hit_w;
+          wire  [31:0]   cnt_wb_r;
+          wire  [31:0]   cnt_wb_w;
+          wire [4:0]rs1,rs2;
+          assign i_p_write=MemWriteM;
+          assign i_p_read=(MemWriteM==0);
+          assign i_p_writedata=WriteDataM;
+          assign {rs2,rs1}=ALU_ResultM[11:2];
+          wire [31:0]data_out;
+          wire[31:0]data_in;
+          address dut1(
+     MemWriteM,
+     clk,    
+     rst_n,  
+    (i_p_write||i_p_read),  
+    rs1,
+    rs2,
+    i_p_addr,
+    i_p_byte_en
+);
+// IT WILL NOT WORK WITH CACHE ON BECOUSE THIS IS IP AND THE MEMORY IS EMPTY SO IF YOU WANT TO TRY IT DEACTIVATE THE CACHE
+// AND TIE o_p_waitrequest TO ZERO    
+    cache dut(clk,
+               rst,
+               i_p_addr,
+               i_p_byte_en,
+               i_p_writedata,
+               i_p_read,
+               i_p_write,
+               ReadDataM,
+               o_p_readdata_valid,
+               o_p_waitrequest,
+               o_m_addr,
+               o_m_byte_en,
+               data_in,
+               o_m_read,
+               o_m_write,
+               data_out,
+               i_m_readdata_valid,
+               i_m_waitrequest,
+               cnt_r,
+               cnt_w,
+               cnt_hit_r,
+               cnt_hit_w,
+               cnt_wb_r,
+               cnt_wb_w);
     Data_Memory dmem (
                         .clk(clk),
                         .rst(rst),
-                        .WE(MemWriteM),
-                        .WD(WriteDataM),
+                        .WE(o_m_write),
+                        .WD(data_in),
                         .i_m_readdata_valid(i_m_readdata_valid),
                         .i_m_waitrequest(i_m_waitrequest),
                         .ren(o_m_read),
                         .A(ALU_ResultM),
-                        .RD(ReadDataM)
+                        .RD(data_out)
                     );
+
+    // Data_Memory dmem (
+    //                     .clk(clk),
+    //                     .rst(rst),
+    //                     .WE(MemWriteM),
+    //                     .WD(WriteDataM),
+    //                     .i_m_readdata_valid(i_m_readdata_valid),
+    //                     .i_m_waitrequest(i_m_waitrequest),
+    //                     .ren(o_m_read),
+    //                     .A(ALU_ResultM),
+    //                     .RD(ReadDataM)
+    //                 );
 
     always @(posedge clk or negedge rst) begin
         if (rst == 1'b0) begin
