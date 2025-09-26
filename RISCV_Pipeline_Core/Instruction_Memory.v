aa61a280a03a63b56f4de6430dec886f6432d365
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-09-26 04:20:24 +0300

    Update Instruction_Memory.v

--- RISCV_Pipeline_Core/Instruction_Memory.v
+++ RISCV_Pipeline_Core/Instruction_Memory.v
@@ -4,34 +4,16 @@ module Instruction_Memory(input rst,
   input o_p_waitrequest
 );
 
-  
   (* rom_style = "block" *)reg [31:0] mem [1023:0];
   
-  (* keep = "true" *) assign RD = (rst==1'b0)?32'd0:mem[A[31:2]];
+
+  wire [9:0] addr_index;
+  assign addr_index = A[11:2];
+  
+  (* keep = "true" *) assign RD = (rst==1'b0) ? 32'd0 : mem[addr_index];
 
   initial begin
     $readmemh("memfile.hex", mem);
   end
-// integer i=0;
-// always@(*)begin
-//  mem[0]=32'h00500293;   //  0100011
-//  mem[1]=32'h00300313;     //0110011_00101_00000_000_00101_0100011
-//  mem[2]=32'h006283B3;
-// mem[3]=32'h00002403;
-//  mem[4]=32'h00100493;
-// mem[5]=32'h00940533;
-// for(i=0;i<1019;i=i+1)
-// mem[i]=32'd0;
-// end
-/*
-  initial begin
-    //mem[0] = 32'hFFC4A303;
-    //mem[1] = 32'h00832383;
-    // mem[0] = 32'h0064A423;
-    // mem[1] = 32'h00B62423;
-    mem[0] = 32'h0062E233;
-    // mem[1] = 32'h00B62423;
 
-  end
-*/
 endmodule
