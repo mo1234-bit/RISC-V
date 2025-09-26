a3c7aba337149bf24bbad60b9d9c1e5ba1db2b18
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-09-26 04:17:33 +0300

    Update ALU_Decoder.v

--- RISCV_Pipeline_Core/ALU_Decoder.v
+++ RISCV_Pipeline_Core/ALU_Decoder.v
@@ -14,36 +14,28 @@ module ALU_Decoder( input [1:0]ALUOp,
         (ALUOp == 2'b01) ? 4'b0001 :
         
         // ALUOp = 10: R-type instructions
-        (ALUOp == 2'b10) ? (
-            (funct3 == 3'b000) ? 
-                ({op[5], funct7[5]} == 2'b11) ? 4'b0001 : 4'b0000 
-            ) :
-            (funct3 == 3'b001) ? 4'b0111 :  // SLL
-            (funct3 == 3'b010) ? 4'b0101 :  // SLT
-            (funct3 == 3'b011) ? 4'b0110 :  // SLTU
-            (funct3 == 3'b100) ? 4'b0100 :  // XOR
-            (funct3 == 3'b101) ? (  // SRL/SRA
-                (funct7[5] == 1'b1) ? 4'b1001 : 4'b1000  // SRA : SRL
-            ) :
-            (funct3 == 3'b110) ? 4'b0011 :  // OR
-            (funct3 == 3'b111) ? 4'b0010 :  // AND
-            4'b0000  // Default ADD
-        ) :
+        (ALUOp == 2'b10&&funct3 == 3'b000&&({op[5], funct7[5]} == 2'b11)) ? 4'b0001:
+            (ALUOp == 2'b10&&funct3 == 3'b001) ? 4'b0111 :  // SLL
+            (ALUOp == 2'b10&&funct3 == 3'b010) ? 4'b0101 :  // SLT
+            (ALUOp == 2'b10&&funct3 == 3'b011) ? 4'b0110 :  // SLTU
+            (ALUOp == 2'b10&&funct3 == 3'b100) ? 4'b0100 :  // XOR
+            (ALUOp == 2'b10&&funct3 == 3'b101&&funct7[5] == 1'b1) ?4'b1001:
+            (ALUOp == 2'b10&&funct3 == 3'b101&&!funct7[5] == 1'b1)? 4'b1000:  
+            (ALUOp == 2'b10&&funct3 == 3'b110) ? 4'b0011 :  // OR
+            (ALUOp == 2'b10&&funct3 == 3'b111) ? 4'b0010 :  // AND
         
-        (ALUOp == 2'b11) ? (
-            (funct3 == 3'b000) ? 4'b0000 :  // ADDI
-            (funct3 == 3'b001) ? 4'b0111 :  // SLLI
-            (funct3 == 3'b010) ? 4'b0101 :  // SLTI
-            (funct3 == 3'b011) ? 4'b0110 :  // SLTIU
-            (funct3 == 3'b100) ? 4'b0100 :  // XORI
-            (funct3 == 3'b101) ? (  // SRLI/SRAI
-                (funct7[5] == 1'b1) ? 4'b1001 : 4'b1000  // SRAI : SRLI
-            ) :
-            (funct3 == 3'b110) ? 4'b0011 :  // ORI
-            (funct3 == 3'b111) ? 4'b0010 :  // ANDI
-            4'b0000  // Default ADD
-        ) :
+            
+            ((ALUOp == 2'b11)&&funct3 == 3'b000) ? 4'b0000 :  // ADDI
+            ((ALUOp == 2'b11)&&funct3 == 3'b001) ? 4'b0111 :  // SLLI
+            ((ALUOp == 2'b11)&&funct3 == 3'b010) ? 4'b0101 :  // SLTI
+            ((ALUOp == 2'b11)&&funct3 == 3'b011) ? 4'b0110 :  // SLTIU
+            ((ALUOp == 2'b11)&&funct3 == 3'b100) ? 4'b0100 :  // XORI
+            ((ALUOp == 2'b11)&&funct3 == 3'b101&&(funct7[5] == 1'b1)) ? 4'b1001: // SRLI/SRAI
+            ((ALUOp == 2'b11)&&funct3 == 3'b101&&!(funct7[5] == 1'b1)) ?  4'b1000 :
+            ((ALUOp == 2'b11)&&funct3 == 3'b110) ? 4'b0011 :  // ORI
+            ((ALUOp == 2'b11)&&funct3 == 3'b111) ? 4'b0010   // ANDI
+            :4'b0000;  // Default ADD
         
-        4'b0000;  // Default ADD
+        
 
 endmodule
