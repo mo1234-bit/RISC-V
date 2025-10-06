a128ec3e49537bd07dbf1f26d11965cb6e425b52
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-10-06 13:37:08 +0300

    Update Hazard_unit.v

--- RISCV_Pipeline_Core/Hazard_unit.v
+++ RISCV_Pipeline_Core/Hazard_unit.v
@@ -1,27 +1,35 @@
 module hazard_unit( input rst, RegWriteM, RegWriteW, FRegWriteM, FRegWriteW,
-    input [4:0] RD_M, RD_W, RD_E, Rs1_E, Rs2_E, 
-    input ResultSrcE, 
+    input [4:0] RD_M, RD_W, RD_E, Rs1_E, Rs2_E,  
+    input ResultSrcE, PCSrcE,
     output [1:0] ForwardAE, ForwardBE,
-    output StallF, StallD, FlushE); 
+    output StallF, StallD, FlushE,FlushD); 
 
     wire lwStall; 
     
- 
+   
     assign ForwardAE = (rst == 1'b0) ? 2'b00 : 
+                      
                        ((RegWriteM == 1'b1) & (RD_M != 5'h00) & (RD_M == Rs1_E)) ? 2'b10 :
+                    
                        ((RegWriteW == 1'b1) & (RD_W != 5'h00) & (RD_W == Rs1_E) & 
                         ~((RegWriteM == 1'b1) & (RD_M != 5'h00) & (RD_M == Rs1_E))) ? 2'b01 : 
                        2'b00;
                        
     assign ForwardBE = (rst == 1'b0) ? 2'b00 : 
+                      
                        ((RegWriteM == 1'b1) & (RD_M != 5'h00) & (RD_M == Rs2_E)) ? 2'b10 :
+                       
                        ((RegWriteW == 1'b1) & (RD_W != 5'h00) & (RD_W == Rs2_E) & 
                         ~((RegWriteM == 1'b1) & (RD_M != 5'h00) & (RD_M == Rs2_E))) ? 2'b01 : 
                        2'b00;
+
+  
     assign lwStall = ((ResultSrcE == 1'b1) & 
                      ((RD_E == Rs1_E) | (RD_E == Rs2_E)) & 
                      (RD_E != 5'h00));
-
+     
+     assign FlushD=PCSrcE;
+    
     assign StallF = lwStall;
     assign StallD = lwStall;
     assign FlushE = lwStall;
