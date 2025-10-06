06e3317f8dcca1abad18ca33e8b9c0d7ed04c9df
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-10-06 14:26:59 +0300

    Update ALU_Decoder.v

--- RISCV_Pipeline_Core/ALU_Decoder.v
+++ RISCV_Pipeline_Core/ALU_Decoder.v
@@ -16,7 +16,7 @@ module ALU_Decoder( input [1:0]ALUOp,
             (ALUOp == 2'b10&&funct3 == 3'b011) ? 4'b0110 :
             (ALUOp == 2'b10&&funct3 == 3'b100) ? 4'b0100 :  
             (ALUOp == 2'b10&&funct3 == 3'b101&&funct7[5] == 1'b1) ?4'b1001:
-            (ALUOp == 2'b10&&funct3 == 3'b101&&!funct7[5] == 1'b1)? 4'b1000:  
+            (ALUOp == 2'b10&&funct3 == 3'b101&&funct7[5] == 1'b0)? 4'b1000:  
             (ALUOp == 2'b10&&funct3 == 3'b110) ? 4'b0011 :  
             (ALUOp == 2'b10&&funct3 == 3'b111) ? 4'b0010 :  
             ((ALUOp == 2'b11)&&funct3 == 3'b000) ? 4'b0000 : 
@@ -25,7 +25,7 @@ module ALU_Decoder( input [1:0]ALUOp,
             ((ALUOp == 2'b11)&&funct3 == 3'b011) ? 4'b0110 :  
             ((ALUOp == 2'b11)&&funct3 == 3'b100) ? 4'b0100 :  
             ((ALUOp == 2'b11)&&funct3 == 3'b101&&(funct7[5] == 1'b1)) ? 4'b1001: 
-            ((ALUOp == 2'b11)&&funct3 == 3'b101&&!(funct7[5] == 1'b1)) ?  4'b1000 :
+            ((ALUOp == 2'b11)&&funct3 == 3'b101&&(funct7[5] == 1'b0)) ?  4'b1000 :
             ((ALUOp == 2'b11)&&funct3 == 3'b110) ? 4'b0011 :  
             ((ALUOp == 2'b11)&&funct3 == 3'b111) ? 4'b0010   
             :4'b0000; 
