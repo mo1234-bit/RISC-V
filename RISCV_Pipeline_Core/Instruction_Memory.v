106817491d2a1fe0e683721918737a726ef555cc
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-10-11 22:28:50 +0300

    Update Instruction_Memory.v

--- RISCV_Pipeline_Core/Instruction_Memory.v
+++ RISCV_Pipeline_Core/Instruction_Memory.v
@@ -1,4 +1,4 @@
-module Instruction_Memory(input rst,
+module Instruction_Memory(input rst_n,
   input [31:0]A,
   output [31:0]RD,
   input o_p_waitrequest
@@ -10,7 +10,7 @@ module Instruction_Memory(input rst,
   wire [9:0] addr_index;
   assign addr_index = A[11:2];
   
-  (* keep = "true" *) assign RD = (rst==1'b0) ? 32'd0 : mem[addr_index];
+  (* keep = "true" *) assign RD = (rst_n==1'b0) ? 32'd0 : mem[addr_index];
 
   initial begin
     $readmemh("memfile.hex", mem);
