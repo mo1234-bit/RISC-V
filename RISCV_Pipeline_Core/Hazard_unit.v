d569ba761a5ae69efb05f0dfe25df24643ea5d17
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2026-03-09 02:04:34 +0200

    Refactor hazard detection and forwarding logic

--- RISCV_Pipeline_Core/Hazard_unit.v
+++ RISCV_Pipeline_Core/Hazard_unit.v
@@ -56,56 +56,60 @@ module hazard_unit(
     reg en_J;
     wire lwStall, lwstall2,JumpW_1;
     wire [4:0]Rs1_E_1,RD_M_1;
-    wire Jump_dep=(Rs1_E==RD_M && JumpE);
+    wire jump_rs1_mem_dep=(Rs1_E==RD_M && JumpE);
     // Delayed versions of hazard signal (used for store forwarding)
-    wire w1,w2,w3,w4,w5,w6;
+    wire cache_write_read_conflict1,cache_write_read_conflict2,cache_write_read_conflict3,cache_write_read_conflict4,cache_write_read_conflict5,cache_write_read_conflict6;
 
 
     //when a load comes after store then an flouting point operation is executing we need to forward the result of loading
-    wire load_after_store_forwarding1,load_after_store_forwarding2,load_after_store_forwarding3,
+    wire load_after_store_forwarding1,load_after_store_forwarding2,load_after_store_forwarding3;
     wire load_after_store_forwarding4,load_after_store_forwarding5;
-      wire s_1, s_2, s_3, s_4, s_5,en_forward_1;
+      wire store_after_load_dep_1, store_after_load_dep_2, store_after_load_dep_3, store_after_load_dep_4, store_after_load_dep_5,fp_store_load_fwd_en_1;
 
  wire load_after_store_forwarding=(InstrDM[6:0]==7'b0000111 && InstrDM[14:12]==3'b010 && InstrDB[6:0]==7'b0100111 &&
   InstrDB[14:12]==3'b010)?1:0;
 
-    wire en_forward=((DRD_W == Rs1_D || DRD_W == Rs2_D)&& load_after_store_forwarding5 )?1:0;
+    wire fp_store_load_fwd_en=((DRD_W == Rs1_D || DRD_W == Rs2_D)&& load_after_store_forwarding5 )?1:0;
 
     // when a load is follwed by another load then an integer operation that used the rd of the two prev loades we need to forward 
-    wire forward=(RD_W==Rs1_E)&& (RD_M==Rs1_E || RD_M==Rs2_E) && Rs1_E!=5'd0 &&InstrDB!=InstrDM&&
+    wire double_load_wb_fwd_rs1=(RD_W==Rs1_E)&& (RD_M==Rs1_E || RD_M==Rs2_E) && Rs1_E!=5'd0 &&InstrDB!=InstrDM&&
     (InstrDB[6:0]==7'b0000011 && InstrDM[6:0]==7'b0000011 && InstrDB[14:12]==3'b010 &&InstrDM[14:12]==3'b010);
 
-    wire forward_b=( RD_W==Rs2_E)&& (RD_M==Rs2_E ||RD_M==Rs1_E ) && Rs2_E!=5'd0&& InstrDB!=InstrDM&&
+    wire double_load_wb_fwd_rs2=( RD_W==Rs2_E)&& (RD_M==Rs2_E ||RD_M==Rs1_E ) && Rs2_E!=5'd0&& InstrDB!=InstrDM&&
     (InstrDB[6:0]==7'b0000011 && InstrDM[6:0]==7'b0000011&& InstrDB[14:12]==3'b010 &&InstrDM[14:12]==3'b010);
 
  // when there is a chain of loades followed each followed with an interger operation depend on it's prev load LW ->INT ->LW ->INT 
  //so we need to stall and then forward
 
-  wire p_1,p_2,p_3,p_4,p2_1,p2_2,p2_3,p2_4,p_5,p_6,p_7,p2_5,p2_6,p2_7;
+  wire consec_load_fwd_rs1_1,consec_load_fwd_rs1_2,consec_load_fwd_rs1_3,consec_load_fwd_rs1_4,consec_load_fwd_rs2_1;
+  wire consec_load_fwd_rs2_2,consec_load_fwd_rs2_3,consec_load_fwd_rs2_4,consec_load_fwd_rs1_5,consec_load_fwd_rs1_6;
+  wire consec_load_fwd_rs1_7,consec_load_fwd_rs2_5,consec_load_fwd_rs2_6,consec_load_fwd_rs2_7;
    
    
 
-wire p1=(ResultSrcE  && ResultSrcW) && !FResultSrcE&&
+wire consec_load_fwd_rs1=(ResultSrcE  && ResultSrcW) && !FResultSrcE&&
         ((RD_E == Rs1_D) ) && InstrDM!=InstrDB&&
         (RD_E != 5'h00) && (RD_W==Rs1_M) ;
 
-wire p2=(ResultSrcE && ResultSrcW) &&
+wire consec_load_fwd_rs2=(ResultSrcE && ResultSrcW) &&
         ( (RD_E == Rs2_D)) && InstrDM!=InstrDB&&
         (RD_E != 5'h00) && ( RD_W==Rs2_M ) ; 
 
 // when an Integer operation depend on two prev LW operations so we need to stall both
-wire m=(ResultSrcM  && ResultSrcE && !ResultSrcD)&& InstrDM!=InstrDE&&
+wire dual_load_src_dep=(ResultSrcM  && ResultSrcE && !ResultSrcD)&& InstrDM!=InstrDE&&
 ((Rs1_D==RD_E && Rs2_D==RD_M) || (Rs2_D==RD_E||Rs1_D==RD_M) && (Rs2_D!=Rs1_D));
 
 // this is an load hazard with this sequence sw -> load depended(sw) -> integer depended(lw) operation not coverd in lwstall
-wire o=(InstrDE[6:0]==7'b0000011 && 
+wire sw_lw_int_chain_hazard=(InstrDE[6:0]==7'b0000011 && 
         ((RD_E == Rs1_D) || (RD_E == Rs2_D)) &&
         (RD_E != 5'h00) && InstrDM[6:0]==7'b0100011 && counter==3&&
        ( (DRD_W == Rs1_D || DRD_W == Rs2_D))) && !lwstall2;
 
 //this for a load hazaed which is not covered in lwstall 
- wire a1,a2,a3,a4,a5,a6,a7;
-wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
+ wire load_hazard_late1 ,load_hazard_late2 ,load_hazard_late3,load_hazard_late4,
+ load_hazard_late5,load_hazard_late6,load_hazard_late7;
+
+wire load_hazard_late = ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
         ((RD_E == Rs1_D) || (RD_E == Rs2_D)) &&
         (RD_E != 5'h00) ) &&!lwstall2 && counter1!=5'd0 && counter_1!=2;
     // ---------------------------------------------------------
@@ -122,7 +126,10 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
         (((RegWriteW ) &&
          (RD_W != 5'h00) &&
          (RD_W == Rs1_E) &&
-         !ResultSrcM))||forward || (p_4 || p_5 || p_6 || p_7)||(w3||w4||w5||w6)||(a4||a5||a6||a7)? 2'b01 :
+         !ResultSrcM))||double_load_wb_fwd_rs1 || (consec_load_fwd_rs1_4 || consec_load_fwd_rs1_5
+          || consec_load_fwd_rs1_6 || consec_load_fwd_rs1_7)||
+         (cache_write_read_conflict3||cache_write_read_conflict4||cache_write_read_conflict5||cache_write_read_conflict6)
+         ||(load_hazard_late4||load_hazard_late5||load_hazard_late6||load_hazard_late7)? 2'b01 :
         2'b00;
 
     // ---------------------------------------------------------
@@ -140,7 +147,8 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
           (RD_W != 5'h00) &&
           (RD_W == Rs2_E) &&
           !ResultSrcM) ||
-          (s_5 && is_store_1)) ||forward_b || (p2_4 || p2_5 || p2_6 || p2_7)? 2'b01 :
+          (store_after_load_dep_5 && is_store_1)) ||double_load_wb_fwd_rs2 || (consec_load_fwd_rs2_4 ||
+           consec_load_fwd_rs2_5 || consec_load_fwd_rs2_6 || consec_load_fwd_rs2_7)? 2'b01 :
         2'b00;
 
  // ---------------------------------------------------------
@@ -157,7 +165,7 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
         ((( FRegWriteW) &&
          (RD_W != 5'h00) &&
          (RD_W == Rs1_E) &&
-         !ResultSrcM)|| en_forward_1) ? 2'b01 :
+         !ResultSrcM)|| fp_store_load_fwd_en_1) ? 2'b01 :
         2'b00;
 
     // ---------------------------------------------------------
@@ -175,7 +183,7 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
           (RD_W != 5'h00) &&
           (RD_W == Rs2_E) &&
           !ResultSrcM) ||
-          (s_5 && is_store_1)) ? 2'b01 :
+          (store_after_load_dep_5 && is_store_1)) ? 2'b01 :
         2'b00;
 
 
@@ -183,10 +191,11 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
     // Detect store depending on a previous load (opcode-based)
     // Used to handle memory hazards correctly
     // ---------------------------------------------------------
-    wire s = ((ResultSrcE == 1'b1) &&
+    wire store_after_load_dep = ((ResultSrcE == 1'b1) &&
              (
-              (InstrD[6:0]  == 7'b0100011 && InstrDE[6:0] == 7'b0000011  &&InstrDE[14:12]==3'b010  &&InstrD[14:12]==3'b010) || // SW after LW
-              (InstrDE[6:0] == 7'b0000111 && InstrD[6:0]  == 7'b0100111  &&InstrDE[14:12]==3'b010  &&InstrD[14:12]==3'b010)     // FSW after FLW
+              (InstrD[6:0]  == 7'b0100011 && InstrDE[6:0] == 7'b0000011  &&InstrDE[14:12]==3'b010
+                &&InstrD[14:12]==3'b010) || (InstrDE[6:0] == 7'b0000111 && InstrD[6:0]  == 7'b0100111 
+                 &&InstrDE[14:12]==3'b010  &&InstrD[14:12]==3'b010)     // FSW after FLW
              ) &&
              ((RD_E == Rs1_D) || (RD_E == Rs2_D)) &&
              (RD_E != 5'h00));
@@ -200,7 +209,8 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
        ( (ResultSrcE == 1'b1) &&
         ((RD_E == Rs1_D) || (RD_E == Rs2_D)) &&
         (RD_E != 5'h00) &&
-       ( (DRD_W != Rs1_D && DRD_W != Rs2_D)||p1||p2||m))||o||a;
+       ( (DRD_W != Rs1_D && DRD_W != Rs2_D)||consec_load_fwd_rs1||consec_load_fwd_rs2||dual_load_src_dep))
+       ||sw_lw_int_chain_hazard||load_hazard_late;
    
     // ---------------------------------------------------------
     // Stall due to memory latency or store waiting for data 
@@ -215,9 +225,9 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
     // Pipeline flush and stall control
     // ---------------------------------------------------------
     assign FlushD = PCSrcE && (InstrDE[6:0]!=7'b1100011);              // Flush Decode on branch/jump
-    assign pass   = ((lwStall && !lwstall2&& !o&&!a ) || (counter_2==1)||
+    assign pass   = ((lwStall && !lwstall2&& !sw_lw_int_chain_hazard&&!load_hazard_late ) || (counter_2==1)||
                      (is_FOP && finish1)) ? 1'b1 : 1'b0;
-    assign pass_load=o||a;
+    assign pass_load=sw_lw_int_chain_hazard||load_hazard_late;
     assign StallF = lwStall || lwstall2; // Stall Fetch
     assign StallD = lwStall || lwstall2; // Stall Decode
     assign FlushE = lwStall || lwstall2; // Flush Execute
@@ -247,7 +257,7 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
                 en <= 1'b1;
 
             // Disable counter after timeout or hazard resolution
-            if (counter1 > 5'd17 || s )
+            if (counter1 > 5'd17 || store_after_load_dep )
                 en <= 1'b0;
 
             // Increment counter while enabled
@@ -255,11 +265,11 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
                 counter1 <= counter1 + 1'b1;
 
             // Reset counter after completion or hazard
-            if (counter1 == 5'd19 || s || forward ||m)
+            if (counter1 == 5'd19 || store_after_load_dep || double_load_wb_fwd_rs1 ||dual_load_src_dep)
                 counter1 <= 5'd0;
         end
 
-        if(o||a)
+        if(sw_lw_int_chain_hazard||load_hazard_late)
         en_1<=1;
 
         if(en_1)
@@ -276,7 +286,7 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
             regfile_en<=0;
         end
         else begin
-           if(Jump_dep)
+           if(jump_rs1_mem_dep)
            en_J<=1;
 
            if(en_J)begin
@@ -291,46 +301,46 @@ wire a= ( (ResultSrcE == 1'b1) &&InstrD[6:0]!= 7'b1100011&&
     // ---------------------------------------------------------
     // Delay hazard signal for store forwarding alignment
     // ---------------------------------------------------------
-    D #(1) flip  (.in(s),   .out(s_1), .clk(clk), .rst_n(rst_n));
-    D #(1) flip1 (.in(s_1), .out(s_2), .clk(clk), .rst_n(rst_n));
-    D #(1) flip2 (.in(s_2), .out(s_3), .clk(clk), .rst_n(rst_n));
-    D #(1) flip3 (.in(s_3), .out(s_4), .clk(clk), .rst_n(rst_n));
-    D #(1) flip4 (.in(s_4), .out(s_5), .clk(clk), .rst_n(rst_n));
+    D #(1) flip  (.in(store_after_load_dep),   .out(store_after_load_dep_1), .clk(clk), .rst_n(rst_n));
+    D #(1) flip1 (.in(store_after_load_dep_1), .out(store_after_load_dep_2), .clk(clk), .rst_n(rst_n));
+    D #(1) flip2 (.in(store_after_load_dep_2), .out(store_after_load_dep_3), .clk(clk), .rst_n(rst_n));
+    D #(1) flip3 (.in(store_after_load_dep_3), .out(store_after_load_dep_4), .clk(clk), .rst_n(rst_n));
+    D #(1) flip4 (.in(store_after_load_dep_4), .out(store_after_load_dep_5), .clk(clk), .rst_n(rst_n));
     D #(1) flip5  (.in(load_after_store_forwarding),   .out(load_after_store_forwarding1), .clk(clk), .rst_n(rst_n));
     D #(1) flip6 (.in(load_after_store_forwarding1), .out(load_after_store_forwarding2), .clk(clk), .rst_n(rst_n));
     D #(1) flip7 (.in(load_after_store_forwarding2), .out(load_after_store_forwarding3), .clk(clk), .rst_n(rst_n));
     D #(1) flip8 (.in(load_after_store_forwarding3), .out(load_after_store_forwarding4), .clk(clk), .rst_n(rst_n));
     D #(1) flip9 (.in(load_after_store_forwarding4), .out(load_after_store_forwarding5), .clk(clk), .rst_n(rst_n));
-    D #(1) flip10 (.in(en_forward), .out(en_forward_1), .clk(clk), .rst_n(rst_n));
+    D #(1) flip10 (.in(fp_store_load_fwd_en), .out(fp_store_load_fwd_en_1), .clk(clk), .rst_n(rst_n));
     D #(5) flip11 (.in(Rs1_E), .out(Rs1_E_1), .clk(clk), .rst_n(rst_n));
     D #(5) flip12 (.in(RD_M), .out(RD_M_1), .clk(clk), .rst_n(rst_n));
     D #(1) flip13 (.in(JumpW), .out(JumpW_1), .clk(clk), .rst_n(rst_n));
-    D #(1) flip14 (.in(p1), .out(p_1), .clk(clk), .rst_n(rst_n));
-    D #(1) flip15 (.in(p_1), .out(p_2), .clk(clk), .rst_n(rst_n));
-    D #(1) flip16 (.in(p_2), .out(p_3), .clk(clk), .rst_n(rst_n));
-    D #(1) flip17 (.in(p_3), .out(p_4), .clk(clk), .rst_n(rst_n));
-    D #(1) flip18 (.in(p_4), .out(p_5), .clk(clk), .rst_n(rst_n));
-    D #(1) flip19 (.in(p_5), .out(p_6), .clk(clk), .rst_n(rst_n));
-    D #(1) flip20 (.in(p_6), .out(p_7), .clk(clk), .rst_n(rst_n));
-    D #(1) flip21 (.in(p2), .out(p2_1), .clk(clk), .rst_n(rst_n));
-    D #(1) flip22 (.in(p2_1), .out(p2_2), .clk(clk), .rst_n(rst_n));
-    D #(1) flip23 (.in(p2_2), .out(p2_3), .clk(clk), .rst_n(rst_n));
-    D #(1) flip24 (.in(p2_3), .out(p2_4), .clk(clk), .rst_n(rst_n));
-    D #(1) flip25 (.in(p2_4), .out(p2_5), .clk(clk), .rst_n(rst_n));
-    D #(1) flip26 (.in(p2_5), .out(p2_6), .clk(clk), .rst_n(rst_n));
-    D #(1) flip27 (.in(p2_6), .out(p2_7), .clk(clk), .rst_n(rst_n));
+    D #(1) flip14 (.in(consec_load_fwd_rs1), .out(consec_load_fwd_rs1_1), .clk(clk), .rst_n(rst_n));
+    D #(1) flip15 (.in(consec_load_fwd_rs1_1), .out(consec_load_fwd_rs1_2), .clk(clk), .rst_n(rst_n));
+    D #(1) flip16 (.in(consec_load_fwd_rs1_2), .out(consec_load_fwd_rs1_3), .clk(clk), .rst_n(rst_n));
+    D #(1) flip17 (.in(consec_load_fwd_rs1_3), .out(consec_load_fwd_rs1_4), .clk(clk), .rst_n(rst_n));
+    D #(1) flip18 (.in(consec_load_fwd_rs1_4), .out(consec_load_fwd_rs1_5), .clk(clk), .rst_n(rst_n));
+    D #(1) flip19 (.in(consec_load_fwd_rs1_5), .out(consec_load_fwd_rs1_6), .clk(clk), .rst_n(rst_n));
+    D #(1) flip20 (.in(consec_load_fwd_rs1_6), .out(consec_load_fwd_rs1_7), .clk(clk), .rst_n(rst_n));
+    D #(1) flip21 (.in(consec_load_fwd_rs2), .out(consec_load_fwd_rs2_1), .clk(clk), .rst_n(rst_n));
+    D #(1) flip22 (.in(consec_load_fwd_rs2_1), .out(consec_load_fwd_rs2_2), .clk(clk), .rst_n(rst_n));
+    D #(1) flip23 (.in(consec_load_fwd_rs2_2), .out(consec_load_fwd_rs2_3), .clk(clk), .rst_n(rst_n));
+    D #(1) flip24 (.in(consec_load_fwd_rs2_3), .out(consec_load_fwd_rs2_4), .clk(clk), .rst_n(rst_n));
+    D #(1) flip25 (.in(consec_load_fwd_rs2_4), .out(consec_load_fwd_rs2_5), .clk(clk), .rst_n(rst_n));
+    D #(1) flip26 (.in(consec_load_fwd_rs2_5), .out(consec_load_fwd_rs2_6), .clk(clk), .rst_n(rst_n));
+    D #(1) flip27 (.in(consec_load_fwd_rs2_6), .out(consec_load_fwd_rs2_7), .clk(clk), .rst_n(rst_n));
     D #(32) flip28 (.in(InstrDB), .out(InstrDB1), .clk(clk), .rst_n(rst_n));
-    D #(1) flip29 (.in(w), .out(w1), .clk(clk), .rst_n(rst_n));
-    D #(1) flip30 (.in(w1), .out(w2), .clk(clk), .rst_n(rst_n));
-    D #(1) flip31 (.in(w2), .out(w3), .clk(clk), .rst_n(rst_n));
-    D #(1) flip32 (.in(w3), .out(w4), .clk(clk), .rst_n(rst_n));
-    D #(1) flip33 (.in(w4), .out(w5), .clk(clk), .rst_n(rst_n));
-    D #(1) flip34 (.in(w5), .out(w6), .clk(clk), .rst_n(rst_n));
-    D #(1) flip35 (.in(a), .out(a1), .clk(clk), .rst_n(rst_n));
-    D #(1) flip36 (.in(a1), .out(a2), .clk(clk), .rst_n(rst_n));
-    D #(1) flip37 (.in(a2), .out(a3), .clk(clk), .rst_n(rst_n));
-    D #(1) flip38 (.in(a3), .out(a4), .clk(clk), .rst_n(rst_n));
-    D #(1) flip39 (.in(a4), .out(a5), .clk(clk), .rst_n(rst_n));
-    D #(1) flip40 (.in(a5), .out(a6), .clk(clk), .rst_n(rst_n));
-    D #(1) flip41 (.in(a6), .out(a7), .clk(clk), .rst_n(rst_n));
+    D #(1) flip29 (.in(w), .out(cache_write_read_conflict1), .clk(clk), .rst_n(rst_n));
+    D #(1) flip30 (.in(cache_write_read_conflict1), .out(cache_write_read_conflict2), .clk(clk), .rst_n(rst_n));
+    D #(1) flip31 (.in(cache_write_read_conflict2), .out(cache_write_read_conflict3), .clk(clk), .rst_n(rst_n));
+    D #(1) flip32 (.in(cache_write_read_conflict3), .out(cache_write_read_conflict4), .clk(clk), .rst_n(rst_n));
+    D #(1) flip33 (.in(cache_write_read_conflict4), .out(cache_write_read_conflict5), .clk(clk), .rst_n(rst_n));
+    D #(1) flip34 (.in(cache_write_read_conflict5), .out(cache_write_read_conflict6), .clk(clk), .rst_n(rst_n));
+    D #(1) flip35 (.in(load_hazard_late), .out(load_hazard_late1 ), .clk(clk), .rst_n(rst_n));
+    D #(1) flip36 (.in(load_hazard_late1 ), .out(load_hazard_late2 ), .clk(clk), .rst_n(rst_n));
+    D #(1) flip37 (.in(load_hazard_late2 ), .out(load_hazard_late3), .clk(clk), .rst_n(rst_n));
+    D #(1) flip38 (.in(load_hazard_late3), .out(load_hazard_late4), .clk(clk), .rst_n(rst_n));
+    D #(1) flip39 (.in(load_hazard_late4), .out(load_hazard_late5), .clk(clk), .rst_n(rst_n));
+    D #(1) flip40 (.in(load_hazard_late5), .out(load_hazard_late6), .clk(clk), .rst_n(rst_n));
+    D #(1) flip41 (.in(load_hazard_late6), .out(load_hazard_late7), .clk(clk), .rst_n(rst_n));
 endmodule
